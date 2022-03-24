9b9f28ba2e59e9be5fab8a6613e0f833cf9833ee
reza.dehnavi <reza.dehnavi@momox.biz>
2022-03-24 09:25:39 +0100

    Add CSV library

--- Package.swift
+++ Package.swift
@@ -12,11 +12,12 @@ let package = Package(
         .package(url: "https://github.com/kylef/Commander.git", from: "0.9.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/JohnSundell/Files", from: "4.0.2"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+        .package(url: "https://github.com/swiftcsv/SwiftCSV", from: "0.6.1")
     ],
     targets: [
         .target(
             name: "swift-spreadsheet-gen",
-            dependencies: ["Commander", "SwiftyJSON", "Files", "Yams"]),
+        dependencies: ["Commander", "SwiftyJSON", "Files", "Yams", "SwiftCSV"]),
     ]
 )
